82d21a32915b57dfc232248e8f1780aba2943b20
Lilya Yenokyan <lilyayenokyan@Lilya-Yenokyans-MacBook-Pro.local>
2026-05-22 17:34:02 +0400

    updated gold tables logic

--- airflow-weather/dags/sql/create_gold_weather_tables.sql
+++ airflow-weather/dags/sql/create_gold_weather_tables.sql
@@ -1,26 +1,69 @@
--- creating the dim_city table
-CREATE TABLE IF NOT EXISTS `weather-project-497009.gold.dim_city` AS
-SELECT DISTINCT   TO_HEX(MD5(CONCAT(city, CAST(latitude AS STRING), CAST(longitude AS STRING), timezone))) AS city_id,
-                                    city,
-                                    latitude,
-                                    longitude,
-                                        timezone
+-- -- creating the dim_city table
+-- CREATE TABLE IF NOT EXISTS `weather-project-497009.gold.dim_city` AS
+-- SELECT DISTINCT   TO_HEX(MD5(CONCAT(city, CAST(latitude AS STRING), CAST(longitude AS STRING), timezone))) AS city_id,
+--                                     city,
+--                                     latitude,
+--                                     longitude,
+--                                         timezone
+-- FROM `weather-project-497009.silver.weather_cleaned`;
+
+
+-- --creating fact_weather table
+-- CREATE TABLE IF NOT EXISTS `weather-project-497009.gold.fact_weather` 
+-- PARTITION BY DATE(ingested_at)
+-- CLUSTER BY city_id, batch_id AS
+-- SELECT
+--  d.city_id,
+--  w.weather_timestamp,
+--  w.temperature_2m,
+--  w.batch_id,
+--  w.ingested_at
+-- FROM `weather-project-497009.silver.weather_cleaned` as w
+-- JOIN `weather-project-497009.gold.dim_city` as d
+-- ON w.city = d.city
+-- AND w.latitude = d.latitude
+-- AND w.longitude = d.longitude
+-- AND w.timezone = d.timezone;
+
+
+
+
+CREATE OR REPLACE TABLE `weather-project-497009.gold.dim_city` AS
+SELECT DISTINCT
+    TO_HEX(
+        MD5(
+            CONCAT(
+                city,
+                CAST(latitude AS STRING),
+                CAST(longitude AS STRING),
+                timezone
+            )
+        )
+    ) AS city_id,
+
+    city,
+    latitude,
+    longitude,
+    timezone
+
 FROM `weather-project-497009.silver.weather_cleaned`;
 
 
---creating fact_weather table
-CREATE TABLE IF NOT EXISTS `weather-project-497009.gold.fact_weather` 
+CREATE OR REPLACE TABLE `weather-project-497009.gold.fact_weather`
 PARTITION BY DATE(ingested_at)
 CLUSTER BY city_id, batch_id AS
+
 SELECT
- d.city_id,
- w.weather_timestamp,
- w.temperature_2m,
- w.batch_id,
- w.ingested_at
-FROM `weather-project-497009.silver.weather_cleaned` as w
-JOIN `weather-project-497009.gold.dim_city` as d
-ON w.city = d.city
-AND w.latitude = d.latitude
-AND w.longitude = d.longitude
-AND w.timezone = d.timezone;
+    d.city_id,
+    w.weather_timestamp,
+    w.temperature_2m,
+    w.batch_id,
+    w.ingested_at
+
+FROM `weather-project-497009.silver.weather_cleaned` AS w
+
+JOIN `weather-project-497009.gold.dim_city` AS d
+    ON w.city = d.city
+    AND w.latitude = d.latitude
+    AND w.longitude = d.longitude
+    AND w.timezone = d.timezone;
---
+++ airflow-weather/dags/sql/mart_daily_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS `weather-project-497009.data_mart.mart_daily_table` PARTITION BY DATE(weather_date) AS
+SELECT d.city_id,
+       city,
+       timezone,
+       DATE(weather_timestamp) as weather_date,
+       ROUND(AVG(temperature_2m) ,2) as average_temperature,
+       MIN(temperature_2m) as min_temperature,
+       MAX(temperature_2m) as max_temperature,
+       COUNT(*) as records_count,
+       MAX(ingested_at) as latest_ingested_at
+FROM `weather-project-497009.gold.fact_weather` as f
+JOIN `weather-project-497009.gold.dim_city` as d
+ON d.city_id=f.city_id
+GROUP BY city_id, city, timezone, weather_date
+ORDER BY city_id, weather_date DESC;
